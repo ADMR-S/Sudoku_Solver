ef73bfee8ea0161155fd3ea0124925e0561bea45
Florent Bélot <106475863+BelotFlorent@users.noreply.github.com>
2024-11-10 15:49:44 +0100

    Delete src/strategy/Visitor.java

--- src/strategy/Visitor.java
+++
@@ -1,9 +0,0 @@
-package strategy;
-import composite.cell.*;
-
-import composite.*;
-
-public interface Visitor {
-    boolean visitEmptyCell(EmptyCell cell, Grid grid);
-    boolean visitCell(Cell cell, Grid grid);
-}
