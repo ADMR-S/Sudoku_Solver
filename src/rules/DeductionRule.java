0ffa17ffc0fc65bd2714bcf0139786e661eb983c
Florent Bélot <106475863+BelotFlorent@users.noreply.github.com>
2024-11-10 13:47:31 +0100

    Delete src/rules directory

--- src/rules/DeductionRule.java
+++
@@ -1,9 +0,0 @@
-package rules;
-import composite.*;
-import composite.cell.CellBase;
-import pile.Pile;
-
-public abstract class DeductionRule {
-    public abstract boolean execut(CellBase c, Grid g);
-    public abstract void routine(Pile p, Grid g);
-}
