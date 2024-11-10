ff23149641886682d86d6deeb06f135174f85bb2
Florent Bélot <106475863+BelotFlorent@users.noreply.github.com>
2024-11-10 23:44:11 +0100

    Update SudokuLogger.java

--- src/log/SudokuLogger.java
+++ src/log/SudokuLogger.java
@@ -4,7 +4,12 @@ import java.util.logging.Logger;
 
 public class SudokuLogger {
     private static final Logger logger = Logger.getLogger(SudokuLogger.class.getName());
-
+    
+    /**
+     * Retourne le logger.
+     *
+     * @return  Logger le logger
+     */
     public static Logger getLogger(){
         return logger;
     }
